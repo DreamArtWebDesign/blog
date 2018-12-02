ae840fb1836fe55ad695fc602b3beb0aa82dadea
caimingA <caiming106@sina.com>
2018-12-03 00:38:39 +0800

    开始

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+//数据库链接
+var link = require("../server/link");
+var blogTbl = require("../server/DB/blog_tbl");
+var tagTbl = require("../server/DB/tag_tbl");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // blogTbl.findAll({
+  //     where:{
+  //       id : 1
+  //     }
+  // }).then(function (result) {
+  //     res.render('index', { title: result[0].title });
+  // }).catch(function (err) {
+  //     console.log("fail : " + err);
+  // });
+
+
+});
+
+module.exports = router;
---
+++ server/DB/blog_tbl.js
@@ -0,0 +1,35 @@
+var Sequelize = require("sequelize");
+var mysql = require("../link");
+var sequelize = mysql.sequelize;
+var Blog = sequelize.define('blog_tbl', {
+
+        id:{ //主键
+            type:Sequelize.INTEGER(11),
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title:{
+            type:Sequelize.STRING(100)
+        },
+        content:{
+            type:Sequelize.STRING(2000)
+        },
+        html:{
+            type:Sequelize.STRING(2000)
+        },
+        date:{
+            type:Sequelize.DATE()
+        },
+        kind:{
+            type:Sequelize.STRING(30)
+        },
+        tag:{
+            type:Sequelize.STRING(30)
+        }
+    },
+    {
+        freezeTableName: true
+    }
+);
+
+module.exports = Blog;
